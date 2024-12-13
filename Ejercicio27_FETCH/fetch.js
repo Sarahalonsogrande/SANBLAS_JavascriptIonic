5ddd7e1288d09955967a2f4231db14ab797dcb52
Sarah Alonso Grande <sarahalonsogrande@gmail.com>
2024-12-13 12:01:38 +0100

    Actualizo ejercicios

--- Ejercicio27_FETCH/fetch.js
+++ Ejercicio27_FETCH/fetch.js
@@ -1,63 +1,64 @@
-// FETCH en Javascript.
-
-// Realizamos una solicitud GET a la API de JSONPlaceholder
 const URL = 'https://fpaniaguaangular.github.io/gamecovers/assets/gamecovers/gamecovers.json';
 
-// function processData(data) { // Data es un ARRAY (En este caso)
 
-//     const CONSOLASNAME =
-//         data.filter(consola => {
-//             return consola.orientation === 'portrait';
-//         });
+function processData(data) {//data (en este caso) es un array
+    //** FILTER */
 
-//     CONSOLASNAME.forEach(consola => {
-//         console.log(consola.name);
-//     });
-// }
-
-function processData(data) { // Data es un ARRAY (En este caso)
-
-    const CONSOLASNAME =
-        data.filter(consola => {
-            return consola.name.includes('Nintendo');
-        });
-
-    CONSOLASNAME.forEach(consola => {
+    /*
+    data.forEach(consola => {
         console.log(consola.name);
     });
+    */
+    /*
+     const consolasVerticales =
+         data.filter(consola => {
+             return consola.orientation === 'portrait';
+         });
+ 
+     consolasVerticales.forEach(consola => {
+         console.log(consola.name);
+     });
+     */
+
+    /*
+   const consolasNintendo = data.filter(consola => {
+       return consola.name.toUpperCase().includes("NINTENDO");
+   });
+   consolasNintendo.forEach(consola => {
+       console.log(consola.name);
+   });
+   */
+
+    /*
+    data.filter(consola => consola.name.toUpperCase().includes("NINTENDO")).
+        forEach(element => console.log(element.name));
+    */
+
+    /** MAP */
+    const consolas = data.map(consola => {
+        const sql = `insert into T_CONSOLAS set (name, value, orientation) values ('${consola.name}','${consola.value}','${consola.orientation}')`
+        return sql;
+    });
+    console.log(consolas);
+
 }
 
 function doRequest(url) {
     fetch(url)
         .then((response) => {
-            if (!response.ok) {
+            //if (!response.ok) {
+            if (response.status != 200) {//200 es OK
+                //console.error(response.status)
                 throw new Error("Network response was not ok");
             }
-            return response.json();
+            //return response.text();//Si estamos obteniendo texto
+            return response.json();//Si estamos obteniendo json
         })
         .then((data) => {
+            //En este punto es donde tenemos los datos que nos proporciona el servidor
             processData(data);
         })
-        .catch((error) => console.error("Fetch error:", error)); // In case of an error, it
+        .catch((error) => console.error("Fetch error:", error));
 }
 
 doRequest(URL);
-
-// Realizamos una solicitud GET a la API de JSONPlaceholder
-// fetch('https://jsonplaceholder.typicode.com/posts')
-//     .then(response => {
-//         // Verificamos si la respuesta es exitosa (status 200-299)
-//         if (!response.ok) {
-//             throw new Error('Error en la solicitud');
-//         }
-//         // Parseamos la respuesta JSON
-//         return response.json();
-//     })
-//     .then(data => {
-//         // Usamos los datos obtenidos
-//         console.log('Datos recibidos:', data);
-//     })
-//     .catch(error => {
-//         // Capturamos y mostramos errores
-//         console.error('Hubo un problema con la solicitud:', error);
-//     });
---
+++ Ejercicio28_FILTER_MAP/Ejercicio28_FILTER_MAP.js
@@ -0,0 +1,54 @@
+// FILTER y MAP
+
+const playa_1 = {
+    nombre : 'Famara',
+    pais : 'España',
+    dificultad : 'Principiante'
+}
+
+const playa_2 = {
+    nombre : 'Nazaré',
+    pais : 'Portugal',
+    dificultad : 'Experto'
+}
+
+const playa_3 = {
+    nombre : 'Pipeline',
+    pais : 'Estados Unidos',
+    dificultad : 'Experto'
+}
+
+const playa_4 = {
+    nombre : 'Somo',
+    pais : 'España',
+    dificultad : 'Intermedio'
+}
+
+// Métodos para agregar objetos a un array:
+// PUSH agrega al final, con UNSHIFT lo agrega al principio.
+let playas = new Array();
+
+playas.push(playa_1);//Agrega al final
+playas.push(playa_2);
+playas.push(playa_3);
+// console.log(playas);//Famara, Nazare, Pipeline
+playas.unshift(playa_4);//Agrega al principio
+// console.log(playas);//Somo, Famara, Nazare, Pipeline
+
+//1. Filter de las playas de dificultad Experto
+
+playas.filter((playa) => playa.dificultad.includes("Experto")).
+forEach(playa => console.log('PLAYAS CON DIFICULTAD EXPERTO: ' + playa.nombre));
+
+//2. Filter de las playas de España
+
+playas.filter(playa => playa.dificultad === 'Experto').forEach(playa => console.log('PLAYAS DE ESPAÑA: ' + playa.nombre));
+
+//3. Obtención del código HTML que representa una playa
+//<div class='playa'><div>Playa:Famara</div><div>País:España</div></div>
+
+let html = playas.map(playa => {
+    return `<div class='playa'><div>Playa: "${playa.nombre}" </div>><div>País: "${playa.pais}" </div></div>`;
+});
+
+console.log(html);
