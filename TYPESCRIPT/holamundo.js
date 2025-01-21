d7dd51736b2d62c2a1efcb73052c6dcd02641fda
Sarah Alonso Grande <sarahalonsogrande@gmail.com>
2025-01-21 13:59:16 +0100

    Iniciamos repositorio

---
+++ TYPESCRIPT/holamundo.js
@@ -0,0 +1,5 @@
+function sumar(num1, num2) {
+    return num1 + num2;
+}
+var resultado = sumar(99, 4);
+console.log(resultado);
---
+++ TYPESCRIPT/holamundo.ts
@@ -0,0 +1,7 @@
+function sumar(num1: number, num2: number): number {
+    return num1 + num2;
+}
+
+let resultado = sumar(99, 4);
+
+console.log(resultado);
