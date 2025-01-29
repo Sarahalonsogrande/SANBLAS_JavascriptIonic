ddf836b05246048569c3226f75affaaf3ad5273b
Sarah Alonso Grande <sarahalonsogrande@gmail.com>
2025-01-29 13:17:08 +0100

    Actualizo ejercicios

--- Proyectos_IONIC/Hola_Mundo_Ionic/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Hola_Mundo_Ionic',
-  webDir: 'www'
-};
-
-export default config;
